a560cd640e34c5019415b92a231d3f0097f1460e
rgonzalezsie <104359772+rgonzalezsie@users.noreply.github.com>
2024-09-03 13:31:18 +0200

    Creando estructura inicial

--- app/src/main/java/com/iessanandres/appuno/MainActivity.kt
+++ app/src/main/java/com/iessanandres/appuno/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.iessanandres.appuno
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,15 +10,28 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.d("MainActivity","onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity","onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity","onStop")
     }
 }
